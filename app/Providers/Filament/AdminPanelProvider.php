1226aa984dbd016e4c02131b5a4d4d0348c260c1
Willy Barrios <romeob-@outlook.com>
2026-04-11 14:57:58 -0600

    Set brand name for admin panel to 'Cinema TV'

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Cinema TV')
             ->colors([
                 'primary' => Color::Amber,
             ])
